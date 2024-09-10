686dcda5829938de13a52e57ac7c90b41bc3e1d1
kavitha Bhandari <kavithabhandari@kavithas-MacBook-Air.local>
2024-09-10 15:50:07 -0400

    Datatypes

---
+++ DTDemo.java
@@ -0,0 +1,20 @@
+package JavaPackage;
+
+public class DTDemo {
+	public static void main(String[] args) {
+		 byte bt=-20;
+		 short sht=125;
+		 int myint=7634;
+		 long lng=76274L;
+		
+		 float myfloat=2.1f;
+		 double mydouble=23.1d;
+		 
+		 boolean bool= true;
+		 char ch='n';
+		 
+		 DTDemo dtd = new DTDemo();
+		 String str = new String();
+	}
+
+}
---
+++ Intro.java
@@ -0,0 +1,15 @@
+package JavaPackage;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This single line comment
+		/*
+		 * This is multiple line comments
+		 */
+		System.out.println("This is my first java program");
+
+	}
+
+}
