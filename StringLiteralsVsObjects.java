85093234a16fb7b972dff2f3c9b5f041124e0f50
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-10 21:39:04 -0400

    String literals VS objects

---
+++ StringLiteralsVsObjects.java
@@ -0,0 +1,13 @@
+package JavaPackage;
+
+public class StringLiteralsVsObjects {
+	public static void main(String[] args) {
+		String str1 = "kavitha";
+		String str2 = new String("kavitha");
+		String str3 = "kavitha";
+		System.out.println(str1==str3);
+		
+	}
+	
+
+}
