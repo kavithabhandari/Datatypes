802ed53fa0a8a148754bad0c44293d3fdc2f5624
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-10 22:11:02 -0400

    variablesdemo

---
+++ variablesdemo.java
@@ -0,0 +1,19 @@
+package JavaPackage;
+
+public class variablesdemo {
+	public static void main(String[] args) {
+		
+		//type variable name=value
+		
+		int num = 11;
+		int num1 = 12;
+		char ch = 'K';
+		String str = "kavitha";
+		
+		System.out.println(str +" " + num); //concat b/w string and num
+		System.out.println(num + num1); //concat bw 2 numbers
+		
+		
+	}
+
+}
