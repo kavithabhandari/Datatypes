b756a03355b53baa09ded602daa38368d4b06ac8
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-17 18:06:38 -0400

    continue

---
+++ continuedemo.java
@@ -0,0 +1,16 @@
+package JavaPackage;
+
+public class continuedemo {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			if(i==8) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("final statement");
+			
+		}
+	}
+
