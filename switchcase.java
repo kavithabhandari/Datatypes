9982f8e2ba9b97488d8224197db886a601ee63da
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-17 12:57:21 -0400

    switchcase

--- switchcase.java
+++ switchcase.java
@@ -5,15 +5,25 @@ public class switchcase {
 	public static void main(String[] args) {
 		int num1 = 20;
 		int num2 = 30;
-	int add = num1 + num2;
+		int result;
+		int add = 1;
 		switch(add) {
 		case 1:
-			System.out.println("Result:"+ (num1 + num2));
-			break;		case 2:
-			System.out.println("Result:" + (num1 - num2));
+			result = num1 + num2;
+			System.out.println("addition: " + result );
 			break;
-			default:
-				System.out.println("Error cases failed");
+		case 2:
+			result = num1 - num2;
+			System.out.println("sub: " + result );
+			break;
+		case 3:
+			result = num1 * num2;
+			System.out.println("mul: " + result );
+			break;
+		case 4:
+			result = num1 % num2;
+			System.out.println("divide: " + result );
+			break;
 			
 			
 			
