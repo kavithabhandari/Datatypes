9db83f423f1a6eecaff6307301bf80ef3f7b2f6d
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-17 11:46:08 -0400

    switchdemo

---
+++ switchcase.java
@@ -0,0 +1,25 @@
+package JavaPackage;
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 30;
+	int add = num1 + num2;
+		switch(add) {
+		case 1:
+			System.out.println("Result:"+ (num1 + num2));
+			break;		case 2:
+			System.out.println("Result:" + (num1 - num2));
+			break;
+			default:
+				System.out.println("Error cases failed");
+			
+			
+			
+		}
+		
+
+	}
+
+}
