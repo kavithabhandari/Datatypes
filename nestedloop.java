7aba96be1460deab959f9b83388760a96ccf7990
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-20 21:54:18 -0400

    nestedloop

---
+++ nestedloop.java
@@ -0,0 +1,22 @@
+package JavaPackage;
+
+public class nestedloop {
+
+	public static void main(String[] args) {
+		int count =0;
+		for(int i=1;i<=4;i++)
+		{
+			int j=1;
+			while(j<=5) {
+				for(int x=1;x<=2;x++) {
+					System.out.println("Value of i is  "  + i  + " value of j is "  + j  + " value of x is "  + x);
+					count ++;
+				}
+				j++;
+				
+			}		
+		}
+		System.out.println(count);
+	}
+
+}
