70ac4443518114dd6c86e98ab518990cd8969f81
kavithabhandari <kavithabhandari10@gmail.com>
2024-09-24 18:50:46 -0400

    array

---
+++ Singledimensionalarray.java
@@ -0,0 +1,17 @@
+package JavaPackage;
+
+public class Singledimensionalarray {
+	public static void main(String[] args) {
+		int[] arraymylist = new int[4]; // Set size to 4
+        arraymylist[0] = 12;
+        arraymylist[1] = 13;
+        arraymylist[2] = 14;
+        arraymylist[3] = 15;
+        
+        System.out.println(arraymylist[3]); // Outputs 15
+        System.out.println(arraymylist.length); // Outputs the size of the array (4)
+ 
+		
+	}
+
+}
